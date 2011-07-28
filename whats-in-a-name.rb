ef61181d94f3d15f2ee8bdda646052e7e981f49c
Murray Steele <muz@h-lame.com>
2011-07-28 18:05:16 +0100

    require datamapper itself rather than just dm-core.  This might mean we don't need to require dm-migrations explicitly either.  We'll see.

--- whats-in-a-name.rb
+++ whats-in-a-name.rb
@@ -1,6 +1,6 @@
 $:.unshift File.dirname(__FILE__)
 
-%w(oa-oauth dm-core dm-postgres-adapter dm-migrations erubis sinatra).each { |dependency| require dependency }
+%w(oa-oauth datamapper dm-postgres-adapter dm-migrations erubis sinatra).each { |dependency| require dependency }
 
 require 'environment'
 require 'models'
