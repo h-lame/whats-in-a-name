29ad4f424a641b225d92f00786af0c5cc1687dca
Murray Steele <muz@h-lame.com>
2011-07-29 11:56:11 +0100

    Test that we don't duplicate twitter details when you re-sign in.

--- test/auth_test.rb
+++ test/auth_test.rb
@@ -6,7 +6,7 @@ class AuthTest < WhatsInANameTest
     app
   end
 
-  def test_i_visit_the_site_and_after_authenticating_with_twitter_it_recognises_me
+  def test_i_visit_the_site_and_after_authenticating_with_twitter_it_stores_my_details_in_the_db
     visit '/'
 
     assert !page.has_content?('robo_dave')
@@ -22,5 +22,21 @@ class AuthTest < WhatsInANameTest
     assert_equal 'Dave McTest', stored_person.name
 
     assert page.has_content?('robo_dave')
+  end
+
+  def test_i_previously_visited_the_site_and_when_i_do_so_again_authenticating_with_twitter_doesnt_duplicate_my_details
+    Person.create(:uid => '12345', :nickname => 'robo_dave', :name => 'Dave McTest', :created_at => Time.now - (3 * 60 * 60 * 24))
+
+    visit '/'
+
+    assert !page.has_content?('robo_dave')
+
+    click_link 'sign in with Twitter'
+
+    assert '/', current_path
+
+    assert page.has_content?('robo_dave')
+
+    assert_equal 1, Person.count(:uid => '12345')
   end
 end
